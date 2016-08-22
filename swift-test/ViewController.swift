f71b7c2d6a718ea1e7e1a454238456561e68bae2
mini <mini@war-room-mac-mini.local>
2016-08-22 16:47:21 +0200

    init

--- swift-test/ViewController.swift
+++ swift-test/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var newButton: UIButton!
+    @IBOutlet var newInput: [UITextField]!
+    var yLabel: Int = 110;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func touchHappend(_ sender: AnyObject) {
+        self.view.addSubview(addLabel(y:yLabel));
+        self.view.addSubview(addSwitch(y:yLabel));
+        
+        yLabel += 35;
+    }
+    
+    func addLabel(y:Int) -> UILabel {
+        let label = UILabel(frame: CGRect(x:150, y:yLabel, width: 80, height: 20));
+        label.text = self.newInput[0].text;
+        label.textAlignment = NSTextAlignment.center;
+        return label;
+    }
+
+    func addSwitch(y:Int) -> UISwitch {
+        let uiSwitch = UISwitch();
+        uiSwitch.frame = CGRect(x:100, y:yLabel, width: 20, height: 10);
+        uiSwitch.isOn = true;
+        return uiSwitch;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
